6ce16b8133c71922891ad919187579712fe1f004
Alexander Kramer <kramer.axs@gmail.com>
2021-03-05 18:49:25 -0600

    [shopPage] adding shop data to firestore and it implementation with redux

--- src/redux/actions/shopActions.js
+++ src/redux/actions/shopActions.js
@@ -1,8 +1,21 @@
-import { shopTypes } from '../types';
+import { shopTypes, appTypes } from '../types';
+import {convertCollectionsSnapshotToMap, firestore} from '../../firebase'; 
 
 export const categorySelected = (category) => (dispatch) => {
   dispatch({
     type: shopTypes.SHOW_CATEGORY_SELECTED,
     payload: category
   })
+}
+
+export const getCollectionsFromFirebase = () => async (dispatch) => {
+
+  dispatch({type: appTypes.LOADING});
+
+  const collectionRef = await firestore.collection('collections');
+  collectionRef.onSnapshot(async snapshot => {
+    const collections = convertCollectionsSnapshotToMap(snapshot);
+    dispatch({type: shopTypes.GET_COLLECTIONS_FIREBASE, payload: collections})
+  });
+
 }

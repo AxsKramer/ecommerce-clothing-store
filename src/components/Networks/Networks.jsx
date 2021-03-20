e6eb377e53bf27d8fd46ce75fcbce91336e8cf6e
Alexander Kramer <kramer.axs@gmail.com>
2021-03-20 12:31:02 -0600

    fix links of social media

--- src/components/Networks/Networks.jsx
+++ src/components/Networks/Networks.jsx
@@ -6,16 +6,16 @@ const Networks = () => {
     <section className="networks-section">
       <h2>Follow us</h2>
       <div className="networks">
-        <a href="/https://www.facebook.com/" title='Facebook' rel="noreferrer" rel="noopener" target='_blank'>
+        <a href="https://www.facebook.com/" title='Facebook' rel="noreferrer" rel="noopener" target='_blank'>
           <i className="fab fa-facebook"></i>
         </a>
-        <a href="/https://www.pinterest.com.mx/" title='Pinterest' rel="noreferrer" rel="noopener" target='_blank'>
+        <a href="https://www.pinterest.com.mx/" title='Pinterest' rel="noreferrer" rel="noopener" target='_blank'>
           <i className="fab fa-pinterest"></i>
         </a>
-        <a href="/https://www.instagram.com/" title='Instagram' rel="noreferrer" rel="noopener" target='_blank'>
+        <a href="https://www.instagram.com/" title='Instagram' rel="noreferrer" rel="noopener" target='_blank'>
           <i className="fab fa-instagram"></i>
         </a>
-        <a href="/https://www.youtube.com/" title='Youtube' rel="noreferrer" rel="noopener" target='_blank'>
+        <a href="https://www.youtube.com/" title='Youtube' rel="noreferrer" rel="noopener" target='_blank'>
           <i className="fab fa-youtube"></i>
         </a>
       </div>
